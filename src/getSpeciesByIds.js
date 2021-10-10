601a06fcf800cdf9a3d57fd2aa6f463f3efbd694
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-10 19:02:24 -0300

    Requisito1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...idsSpecies) {
   // seu código aqui
+  if (idsSpecies === 'undefined') {
+    return [];
+  }
+  const array = [];
+  idsSpecies.forEach((idSpecie) => data.species.filter((value) => {
+    if (value.id === idSpecie) {
+      array.push(value);
+    }
+    return array;
+  }));
+  return array;
 }
-
 module.exports = getSpeciesByIds;
