2739f45157f0589abce0506baff0c35e728a3d75
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-10 21:43:49 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const findSpecie = data.species.filter((value) => value.name === animal);
+  const findAnimalsOfSpecie = findSpecie.map((val) => val.residents);
+  const verificateAgeOfAnimals = findAnimalsOfSpecie.every((value) => value[0].age > age);
+  return verificateAgeOfAnimals;
 }
 
 module.exports = getAnimalsOlderThan;
