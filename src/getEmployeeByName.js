27633f7490c88600319b49568564658a1fe76132
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-10 22:16:24 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+  // Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
+  if (employeeName === undefined) {
+    return {};
+  }
 
+  return data.employees
+    .find((employ) => employ.firstName === employeeName || employ.lastName === employeeName);
+}
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
