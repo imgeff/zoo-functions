a66b98edc6078f83da994150f0e77c7d70eb3385
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-12 13:46:18 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function verificateParam() {
+  const result = {};
+
+  data.species.map((value) => {
+    result[`${value.name}`] = value.residents.length;
+    return result;
+  });
+  return result;
 }
+function countAnimals(animal) {
+  let countAnimalsOfSpecie;
+  if (animal === undefined) {
+    return verificateParam();
+  }
+  const findSpecie = data.species.filter((value) => value.name === animal.specie);
+  if (Object.values(animal).length === 1) {
+    countAnimalsOfSpecie = findSpecie.map((value) => value.residents.length).join();
+    return Number(countAnimalsOfSpecie);
+  }
+  if (Object.values(animal).length === 2) {
+    countAnimalsOfSpecie = findSpecie
+      .map((value) => value.residents
+        .filter((val) => val.sex === animal.sex))[0].length;
+    return countAnimalsOfSpecie;
+  }
+}
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
