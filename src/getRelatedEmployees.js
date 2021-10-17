4042c14d96a072a38d205d97a0c5ada49753ee53
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-17 15:54:12 -0300

    Refatora Req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,16 +5,17 @@ function isManager(id) {
   // será responsável por verificar se uma pessoa colaboradora é gerente ou não. O retorno dessa função deve ser um booleano: true ou false;
   return employees.some((manager) => manager.id === id && manager.managers.length <= 1);
 }
-// console.log(isManager('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
+
 function getRelatedEmployees(managerId) {
   // se for uma pessoa colaboradora gerente, deve retornar um array contendo os nomes das pessoas colaboradoras que ela é responsável;
   // se não for uma pessoa colaboradora gerente, deverá ser lançado um erro gerado com a função construtora Error da biblioteca padrão do JavaScript com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
   if (isManager(managerId) === true) {
-    const man = employees
-      .filter((employ) => employ.managers.find((value) => value === managerId));
-    const manRes = man
+    const personsManaged = employees
+      .filter((employ) => employ.managers.find((manager) => manager === managerId));
+
+    const nameOfPersonsManaged = personsManaged
       .map((value) => `${value.firstName} ${value.lastName}`);
-    return manRes;
+    return nameOfPersonsManaged;
   }
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
