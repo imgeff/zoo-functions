9f5242d156ee00b927bd4a8c5dbd4971b4315f32
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-17 15:18:05 -0300

    Refatora Req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-  const findSpecie = data.species.filter((value) => value.name === animal);
-  const findAnimalsOfSpecie = findSpecie.map((val) => val.residents);
-  const verificateAgeOfAnimals = findAnimalsOfSpecie.every((value) => value[0].age > age);
+  const filterSpecie = data.species.filter((value) => value.name === animal);
+  const allAnimalsOfSpecie = filterSpecie.map((val) => val.residents);
+  const verificateAgeOfAnimals = allAnimalsOfSpecie.every((value) => value[0].age > age);
   return verificateAgeOfAnimals;
 }
-
 module.exports = getAnimalsOlderThan;
